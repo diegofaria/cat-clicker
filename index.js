0910407abe0038f06cb912627b40182d4b7da436
Diego Faria <diegofndefaria@gmail.com>
2016-03-21 13:05:03 -0300

    catDisplay initial

--- index.js
+++ index.js
@@ -1,4 +1,65 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+var React = require('react')
+var ReactDOM = require('react-dom')
+var deepFreeze = require('deep-freeze');
+import expect from 'expect'
+import { createStore } from 'redux'
 
-ReactDOM.render(<h1>Hello React!</h1>, document.getElementById('root'));
+const counter = (state = {name: 'cat 1', image: 'https://www.petfinder.com/wp-content/uploads/2012/11/140272627-grooming-needs-senior-cat-632x475.jpg', counter: 0}, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return Object.assign({}, state, {counter: state.counter + 1})
+        default:
+            return state
+    }
+}
+
+const testIncrementCounter = () => {
+    const stateBefore = {
+        name: '',
+        image: '',
+        counter: 0
+    }
+    const stateAfter = {
+        name: '',
+        image: '',
+        counter: 1
+    }
+
+    deepFreeze(stateBefore)
+
+    expect(stateBefore).toEqual(stateBefore)
+}
+
+testIncrementCounter()
+console.log('All tests passed.')
+
+const store = createStore(counter)
+
+const CatDisplay = ({
+    display,
+    onIncrement
+}) => (
+    <div>
+        <h1>{display.name}</h1>
+        <img src={display.image} onClick={onIncrement}/>
+        <h2>{display.counter}</h2>
+    </div>
+)
+
+console.log(store.getState())
+
+const render = () => {
+    ReactDOM.render(
+        <CatDisplay
+        display={store.getState()}
+        onIncrement={() =>
+            store.dispatch({
+                type: 'INCREMENT'
+            })}
+            />,
+        document.getElementById('root')
+    );
+}
+
+render()
+store.subscribe(render)
