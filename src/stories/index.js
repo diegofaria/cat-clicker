89460b8763603738ecab48a84eb685120899562c
Diego Faria <diegofndefaria@gmail.com>
2016-09-16 08:18:50 -0300

    added catlist story

--- src/stories/index.js
+++ src/stories/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { storiesOf, action, linkTo } from '@kadira/storybook';
 import Button from './Button';
 import CatDisplay from '../CatDisplay';
+import CatList from '../CatList';
 
 const cat = {
   id: 1,
@@ -10,11 +11,31 @@ const cat = {
   image: "http://d39kbiy71leyho.cloudfront.net/wp-content/uploads/2016/05/09170020/cats-politics-TN.jpg"
 }
 
+var cats = [
+  cat,
+  cat,
+  cat,
+  cat
+]
+var id = 0
+
+cats = cats.map(function(item) {
+  id++
+  var novoItem = Object.assign({}, item)
+  novoItem.id = id
+  return novoItem
+})
+
 const incrementCat = (id) => { cat.counter += 1 }
 
 storiesOf('CatDisplay', module)
   .add('with a cat', () => (
     <CatDisplay cat={ {} } onIncrementCat={ incrementCat }/>
+  ))
+
+storiesOf('CatList', module)
+  .add('with four cats', () => (
+    <CatList cats={ cats } />
   ))
 
 storiesOf('Button', module)
